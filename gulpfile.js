5693ca5c6e6945a97285479b93ee695a5b74b6f5
Denes <denes.balog@gmail.com>
2016-03-14 11:52:04 +0100

    - templates added to templateCache

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,20 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('default', function(){
   gulp.src('./angular-socialshare.js')
   .pipe(rename('angular-socialshare.min.js'))
   .pipe(uglify())
   .pipe(gulp.dest('./'));
+
+  gulp.src('templates/**/*.html')
+      .pipe(templateCache('angular-socialshare.tpl.js',{
+        module:'angularSocialShareTemplates',
+        transformUrl:function(url){
+          return "templates/"+url;
+        }
+      }))
+      .pipe(gulp.dest('./'));
 });
